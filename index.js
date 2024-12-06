4f0c7c289aba12af2b33f11cb4849d3d252d183c
Rafi Siddique <rafisiddique67@gmail.com>
2024-12-06 12:39:14 +0600

    intial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://admin:IGBM9HtuR2hTDTxz@cluster0.bk0nm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+//username rafi
+//password PsKom7WqP5Bfi1yh
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+  catch(err) {
+    console.log(err);
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+});
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+});
