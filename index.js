2d988f0c8259c14d8442f0557f38c887791cc1df
Rafi Siddique <rafisiddique67@gmail.com>
2024-12-06 22:21:02 +0600

    post and get added

--- index.js
+++ index.js
@@ -29,10 +29,24 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({
+
+    const crowdCollection = client.db("crowdDB").collection('crowd');
+
+    await client.db("crowdDB").command({
       ping: 1
     });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    app.get('/getCrowd', async (req, res) => {
+      const result = JSON.stringify(client.db('crowdDB').collection('crowd'));
+      res.send(result);
+    })
+
+    app.post('/addCrowd', async (req, res) => {
+      const newCrowd = req.body;
+      const result = await crowdCollection.insertOne(newCrowd);
+      res.send(result);
+    });
   } finally {
     await client.close();
   }
